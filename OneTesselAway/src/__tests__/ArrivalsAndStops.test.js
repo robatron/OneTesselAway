fc693bc769e913ac6d8cfa4a4b501789db97d6bd
robmc <robert.mcgui@gmail.com>
2019-12-29 18:01:00 -0800

    Update unit tests

--- OneTesselAway/src/__tests__/ArrivalsAndStops.test.js
+++ OneTesselAway/src/__tests__/ArrivalsAndStops.test.js
@@ -2,6 +2,7 @@ const {
     extractArrivalsForRoute,
     getArrivalsAndDeparturesForStop,
     getUpcommingArrivalDates,
+    getUpcommingArrivalTimes,
 } = require('../ArrivalsAndStops');
 const { apiKey } = require('../../oba-api-key.json');
 const {
@@ -17,82 +18,92 @@ const DEFAULT_TEST_STOP_ID = '1_12353';
 const DEFAULT_TEST_ROUTE_ID = '1_100009';
 const API_ARRIVALS_AND_DEPARTURES_FOR_STOP = `http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop`;
 
-describe('ArrivalsAndStops', () => {
-    describe('getArrivalsAndDeparturesForStop', () => {
-        it('gets arrival and departure info for specified stop as JSON', async () => {
-            fetch.mockReturnValue(
-                Promise.resolve(
-                    new Response(
-                        JSON.stringify(
-                            DEFAULT_ARRIVALS_AND_DEPARTURES_FOR_STOP_RESPONSE,
-                        ),
-                    ),
+beforeEach(() => {
+    fetch.mockReturnValue(
+        Promise.resolve(
+            new Response(
+                JSON.stringify(
+                    DEFAULT_ARRIVALS_AND_DEPARTURES_FOR_STOP_RESPONSE,
                 ),
-            );
+            ),
+        ),
+    );
+});
 
-            const response = await getArrivalsAndDeparturesForStop(
-                DEFAULT_TEST_STOP_ID,
-            );
+describe('getArrivalsAndDeparturesForStop', () => {
+    it('gets arrival and departure info for specified stop as JSON', async () => {
+        const response = await getArrivalsAndDeparturesForStop(
+            DEFAULT_TEST_STOP_ID,
+        );
 
-            expect(fetch).toHaveBeenCalledTimes(1);
-            expect(fetch).toHaveBeenCalledWith(
-                `${API_ARRIVALS_AND_DEPARTURES_FOR_STOP}/${DEFAULT_TEST_STOP_ID}.json?key=${apiKey}`,
-            );
-            expect(response).toEqual(
-                DEFAULT_ARRIVALS_AND_DEPARTURES_FOR_STOP_RESPONSE,
-            );
-        });
-
-        it('throws an error for non-200 responses', async () => {
-            fetch.mockReturnValue(Promise.resolve({ ok: false }));
-
-            await expect(
-                getArrivalsAndDeparturesForStop(DEFAULT_TEST_STOP_ID),
-            ).rejects.toThrow();
-        });
+        expect(fetch).toHaveBeenCalledTimes(1);
+        expect(fetch).toHaveBeenCalledWith(
+            `${API_ARRIVALS_AND_DEPARTURES_FOR_STOP}/${DEFAULT_TEST_STOP_ID}.json?key=${apiKey}`,
+        );
+        expect(response).toEqual(
+            DEFAULT_ARRIVALS_AND_DEPARTURES_FOR_STOP_RESPONSE,
+        );
     });
 
-    describe('extractArrivalsForRoute', () => {
-        it('extracts arrivals for a specific route', () => {
-            const arrivals = extractArrivalsForRoute(
-                DEFAULT_ARRIVALS_AND_DEPARTURES_FOR_STOP_RESPONSE,
-                DEFAULT_TEST_ROUTE_ID,
-            );
-            expect(arrivals.length).toEqual(2);
-            arrivals.forEach(arrival =>
-                expect(arrival.routeId).toEqual(DEFAULT_TEST_ROUTE_ID),
-            );
-        });
+    it('throws an error for non-200 responses', async () => {
+        fetch.mockReturnValue(Promise.resolve({ ok: false }));
 
-        it('throws an error if the data object is malformed', () => {
-            expect(() => {
-                extractArrivalsForRoute({});
-            }).toThrow();
-        });
+        await expect(
+            getArrivalsAndDeparturesForStop(DEFAULT_TEST_STOP_ID),
+        ).rejects.toThrow();
+    });
+});
+
+describe('extractArrivalsForRoute', () => {
+    it('extracts arrivals for a specific route', () => {
+        const arrivals = extractArrivalsForRoute(
+            DEFAULT_ARRIVALS_AND_DEPARTURES_FOR_STOP_RESPONSE,
+            DEFAULT_TEST_ROUTE_ID,
+        );
+        expect(arrivals.length).toEqual(2);
+        arrivals.forEach(arrival =>
+            expect(arrival.routeId).toEqual(DEFAULT_TEST_ROUTE_ID),
+        );
     });
 
-    describe('getUpcommingArrivalDates', () => {
-        beforeEach(() => {
-            fetch.mockReturnValue(
-                Promise.resolve(
-                    new Response(
-                        JSON.stringify(
-                            DEFAULT_ARRIVALS_AND_DEPARTURES_FOR_STOP_RESPONSE,
-                        ),
-                    ),
-                ),
-            );
-        });
+    it('throws an error if the data object is malformed', () => {
+        expect(() => {
+            extractArrivalsForRoute({});
+        }).toThrow();
+    });
+});
 
-        it('gets upcomming arrival times for a route at a stop', async () => {
-            const upcommingArrivalTimes = await getUpcommingArrivalDates(
-                DEFAULT_TEST_STOP_ID,
-                DEFAULT_TEST_ROUTE_ID,
-            );
-            expect(upcommingArrivalTimes).toEqual([
-                new Date(1577651984000),
-                new Date(1577653615000),
-            ]);
-        });
+describe('getUpcommingArrivalDates', () => {
+    it('gets upcomming arrival times for a route at a stop', async () => {
+        const arrivalsForStop = await getArrivalsAndDeparturesForStop(
+            DEFAULT_TEST_STOP_ID,
+        );
+        const arrivalsForRoute = extractArrivalsForRoute(
+            arrivalsForStop,
+            DEFAULT_TEST_ROUTE_ID,
+        );
+        const upcommingArrivalTimes = getUpcommingArrivalDates(
+            arrivalsForRoute,
+        );
+
+        expect(upcommingArrivalTimes).toEqual([
+            new Date(1577651984000),
+            new Date(1577653615000),
+        ]);
+    });
+});
+
+describe('getUpcommingArrivalTimes', () => {
+    it('returns formatted upcomming arrivial time information', async () => {
+        const relDate = 1577671180380;
+        const upcommingArrivalTimes = await getUpcommingArrivalTimes(
+            DEFAULT_TEST_STOP_ID,
+            DEFAULT_TEST_ROUTE_ID,
+            relDate,
+        );
+        expect(upcommingArrivalTimes).toEqual([
+            { arrivalTime: '12:39', minsUntilArrival: -320 },
+            { arrivalTime: '13:06', minsUntilArrival: -293 },
+        ]);
     });
 });
